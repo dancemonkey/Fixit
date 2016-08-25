e6a1175a42fd069ae97bf9f8e369362db141d63a
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-08-25 09:48:46 -0400

    hooked up the views in the create new VC and starting on getting saving a project to core data

--- Fixit/CreateNewItemVC.swift
+++ Fixit/CreateNewItemVC.swift
@@ -7,21 +7,56 @@
 //
 
 import UIKit
+import CoreData
+import pop
 
 class CreateNewItemVC: UIViewController, UIScrollViewDelegate {
   
   @IBOutlet weak var scrollView: UIScrollView!
   @IBOutlet weak var contentView: UIView!
+  @IBOutlet weak var projectTaskSwitch: UISwitch!
+  @IBOutlet weak var titleFld: UITextField!
+  @IBOutlet weak var timeFld: UITextField!
+  @IBOutlet weak var costFld: UITextField!
+  @IBOutlet weak var dueDate: UIDatePicker!
+  @IBOutlet weak var projectBtnLabel: UILabel!
+  @IBOutlet weak var details: UITextView!
+  @IBOutlet weak var projectSelectBtn: UIButton!
   
   override func viewDidLoad() {
-    super.viewDidLoad()
     
+    super.viewDidLoad()
     scrollView.delegate = self
     
   }
   
-  func scrollViewWillBeginDragging(scrollView: UIScrollView) {
-    print("about to scroll")
+  @IBAction func doneBtnPressed(sender: UIButton) {
+    // test for valid entries and save new project or task to context
+    // for now just testing saving to context so I can get some data into the system
+    
+    do {
+      
+    } catch {
+      print(error)
+    }
+  }
+  
+  @IBAction func selectProjectBtnPressed(sender: UIButton) {
+    
+  }
+  
+  @IBAction func switchChanged(sender: UISwitch) {
+    if sender.on {
+      projectBtnLabel.enabled = true
+      projectSelectBtn.enabled = true
+    } else {
+      projectBtnLabel.enabled = false
+      projectSelectBtn.enabled = false
+    }
+  }
+  
+  @IBAction func cancelPressed(sender: UIButton) {
+    self.navigationController?.popViewControllerAnimated(true)
   }
   
 }
