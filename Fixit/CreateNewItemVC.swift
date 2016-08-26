954de4e74343ca9b2f8147633cd73e023eaca9e0
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-08-25 21:22:41 -0400

    ready to start saving to model. just tinkered with dismissing keyboard when tapping on scroll view

--- Fixit/CreateNewItemVC.swift
+++ Fixit/CreateNewItemVC.swift
@@ -10,6 +10,11 @@ import UIKit
 import CoreData
 import pop
 
+enum ProjectOrTask: String {
+  case Project
+  case Task
+}
+
 class CreateNewItemVC: UIViewController, UIScrollViewDelegate {
   
   @IBOutlet weak var scrollView: UIScrollView!
@@ -23,19 +28,52 @@ class CreateNewItemVC: UIViewController, UIScrollViewDelegate {
   @IBOutlet weak var details: UITextView!
   @IBOutlet weak var projectSelectBtn: UIButton!
   
+  var newCreation: NSManagedObject? = nil
+  var projectOrTask: ProjectOrTask = .Project
+  
   override func viewDidLoad() {
     
     super.viewDidLoad()
+    
+    let notificationCenter = NSNotificationCenter.defaultCenter()
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillHideNotification, object: nil)
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIKeyboardWillChangeFrameNotification, object: nil)
+    
     scrollView.delegate = self
     
+    let tapGest = UITapGestureRecognizer(target: self, action: #selector(CreateNewItemVC.hideKeyboard))
+    tapGest.cancelsTouchesInView = false
+    scrollView.addGestureRecognizer(tapGest)
+    
+  }
+  
+  func hideKeyboard() {
+    self.view.endEditing(true)
+  }
+  
+  func adjustForKeyboard(notification: NSNotification) {
+    let userInfo = notification.userInfo!
+    
+    let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+    let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+    
+    if notification.name == UIKeyboardWillHideNotification {
+      scrollView.contentInset = UIEdgeInsetsZero
+    } else {
+      scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+    }
+    scrollView.scrollIndicatorInsets = scrollView.contentInset
   }
   
   @IBAction func doneBtnPressed(sender: UIButton) {
     // test for valid entries and save new project or task to context
-    // for now just testing saving to context so I can get some data into the system
+    // figure out if it's a project or task
+    // create new entity description
+    // set the values
+    // insert new ns managed object into context
     
     do {
-      
+      try appDelegate.managedObjectContext.save()
     } catch {
       print(error)
     }
@@ -49,9 +87,11 @@ class CreateNewItemVC: UIViewController, UIScrollViewDelegate {
     if sender.on {
       projectBtnLabel.enabled = true
       projectSelectBtn.enabled = true
+      self.projectOrTask = .Task
     } else {
       projectBtnLabel.enabled = false
       projectSelectBtn.enabled = false
+      self.projectOrTask = .Project
     }
   }
   
