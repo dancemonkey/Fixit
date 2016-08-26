89943ec806fb4f52c6e611b98ccbc10393753249
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-08-26 11:45:42 -0400

    projectListVC is working with fetchedResultsController. not pretty or styled but pulling data.

--- Fixit/ProjectListVC.swift
+++ Fixit/ProjectListVC.swift
@@ -14,13 +14,12 @@ class ProjectListVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
   @IBOutlet weak var tableView: UITableView!
   
   lazy var fetchedResultsController: NSFetchedResultsController = {
-    print("getting fetchedResultsC")
     let fetch = NSFetchRequest(entityName: fetches.Projects.rawValue)
     let primarySortDesc = NSSortDescriptor(key: "dueDate", ascending: false)
     let secondarySortDesc = NSSortDescriptor(key: "title", ascending: true)
     fetch.sortDescriptors = [primarySortDesc, secondarySortDesc]
     
-    let frc = NSFetchedResultsController(fetchRequest: fetch, managedObjectContext: appDelegate.managedObjectContext, sectionNameKeyPath: "title", cacheName: nil)
+    let frc = NSFetchedResultsController(fetchRequest: fetch, managedObjectContext: appDelegate.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
     
     frc.delegate = self
     
@@ -30,9 +29,11 @@ class ProjectListVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    tableView.delegate = self
+    tableView.dataSource = self
+    
     do {
       try fetchedResultsController.performFetch()
-      print("performed fetch")
     } catch {
       print("\(error)")
     }
@@ -44,37 +45,24 @@ class ProjectListVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
   }
   
   func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    if let sections = fetchedResultsController.sections {
-      print(sections.count)
-      return sections.count
-    }
     return 1
   }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     if let sections = fetchedResultsController.sections {
       let current = sections[section]
-      print(current.numberOfObjects)
       return current.numberOfObjects
     }
     return 0
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("projectCell")
+    let cell = tableView.dequeueReusableCellWithIdentifier("projectCell", forIndexPath: indexPath)
     let project = fetchedResultsController.objectAtIndexPath(indexPath) as! Project
-    cell?.textLabel?.text = project.title
-    cell?.detailTextLabel?.text = String(project.dueDate)
+    cell.textLabel?.text = project.title
+    cell.detailTextLabel?.text = String(project.dueDate)
     
-    return cell!
-  }
-  
-  func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    if let sections = fetchedResultsController.sections {
-      let current = sections[section]
-      return current.name
-    }
-    return nil
+    return cell
   }
   
 }
