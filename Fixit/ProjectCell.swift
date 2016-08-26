965f0af67fddf959e5b5001bc3f25a5be9b32d55
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-08-26 18:17:36 -0400

    built projectCell in IB and in code, not tested

---
+++ Fixit/ProjectCell.swift
@@ -0,0 +1,43 @@
+//
+//  ProjectCell.swift
+//  Fixit
+//
+//  Created by Drew Lanning on 8/26/16.
+//  Copyright © 2016 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCell: UITableViewCell {
+  
+  @IBOutlet weak var thumbImg: UIImageView!
+  @IBOutlet weak var taskLbl: UILabel!
+  @IBOutlet weak var titleLbl: UILabel!
+  @IBOutlet weak var estCostLbl: UILabel!
+  @IBOutlet weak var estTimeLbl: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+  }
+  
+  func configureCell(withProject project: Project) {
+    let estCostStub = "Est. cost: $"
+    let estTimeStub = "Est. time: "
+    let estTimeUnitStub = " min."
+    
+    if let title = project.title {
+      self.titleLbl.text = title
+    }
+    if let tasks = project.taskList {
+      self.taskLbl.text = String(tasks.count)
+    }
+    if let estCost = project.estimatedCost {
+      self.estCostLbl.text = estCostStub + String(estCost)
+    }
+    if let estTime = project.estimatedTime {
+      self.estTimeLbl.text = estTimeStub + String(estTime) + estTimeUnitStub
+    }
+  }
+  
+}
