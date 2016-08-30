9dc26594611646ef316f2fcd59cff9ac8dc5cf15
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-08-30 19:08:59 -0400

    fancy menu implemented, now add images and text to flyout buttons and create newTask view

--- Fixit/DashboardVC.swift
+++ Fixit/DashboardVC.swift
@@ -9,31 +9,50 @@
 import UIKit
 import CoreData
 import pop
+import CircleMenu
 
-class DashboardVC: UIViewController {
+class DashboardVC: UIViewController, CircleMenuDelegate {
   
   @IBOutlet var dashboardButtons: [UIButton]!
   @IBOutlet weak var projectCell: DashboardCellView!
   @IBOutlet weak var taskCell: DashboardCellView!
   @IBOutlet weak var shoppingListCell: DashboardCellView!
   @IBOutlet weak var hitListCell: DashboardCellView!
-
+  @IBOutlet weak var addNewButton: CircleMenu!
+  
   let segueStrings = ["goToProjects","goToTasks","goToShoppingList","goToHitList"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    addNewButton.delegate = self
   }
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
+    
     projectCell.updateProjectView()
+    
     // TODO: populate Dasboard Cells with data from fetch
     
   }
   
   @IBAction func buttonTapped(sender: UIButton) {
     performSegueWithIdentifier(segueStrings[sender.tag], sender: sender)
+  }
+  
+  func setCircleMenuButtons() {
+    
+  }
+  
+  func circleMenu(circleMenu: CircleMenu, willDisplay button: UIButton, atIndex: Int) {
+    button.addTarget(self, action: #selector(buttonPressedTest(_:)), forControlEvents: .TouchUpInside)
+  }
+  
+  func buttonPressedTest(sender: UIButton) {
+    // now have view change based on button pressed
+    Utils.delay(0.5) {
+      self.performSegueWithIdentifier("newProject", sender: self)
+    }
   }
   
 }
---
+++ Fixit/utils.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  Fixit
+//
+//  Created by Drew Lanning on 8/30/16.
+//  Copyright © 2016 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class Utils {
+  
+  static func delay(delay: Double, closure: ()->()) {
+    dispatch_after(
+      dispatch_time(
+        DISPATCH_TIME_NOW,
+        Int64(delay * Double(NSEC_PER_SEC))
+      ),
+      dispatch_get_main_queue(),
+      closure
+    )
+  }
+  
+  
+}
