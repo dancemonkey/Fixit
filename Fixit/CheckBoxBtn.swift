be6e35ffcc09c8bd8a629062c67b6343047ca6bb
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-09-26 22:21:47 -0400

    added smaller center circle on "checkbox" to indicate completion, rather than fully filled circle

--- Fixit/CheckBoxBtn.swift
+++ Fixit/CheckBoxBtn.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class CheckBoxBtn: UIButton {
   
+  private var _doneIndicator: UIView?
+  
+  var doneIndicator: UIView! {
+    get {
+      if _doneIndicator == nil {
+        let doneInd = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width*0.8, height: self.frame.height*0.8))
+        doneInd.center = convert(self.center, from: self.superview)
+        doneInd.backgroundColor = UIColor(red:0.35, green:0.23, blue:0.00, alpha:1.0)
+        doneInd.layer.cornerRadius = doneInd.frame.width/2
+        doneInd.layer.masksToBounds = true
+        doneInd.isUserInteractionEnabled = false
+        addSubview(doneInd)
+        _doneIndicator = doneInd
+      }
+      return _doneIndicator
+    }
+  }
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     layer.borderWidth = 1.0
@@ -19,12 +37,17 @@ class CheckBoxBtn: UIButton {
   
   func completeTask() {
     Utils.animateButton(self, withTiming: btnAnimTiming) { 
-      self.layer.backgroundColor = UIColor(red:0.35, green:0.23, blue:0.00, alpha:1.0).cgColor
+      //self.layer.backgroundColor = UIColor(red:0.35, green:0.23, blue:0.00, alpha:1.0).cgColor
+      let _ = self.doneIndicator
     }
   }
   
   func incompleteTask() {
-    layer.backgroundColor = UIColor.white.cgColor
+    //layer.backgroundColor = UIColor.white.cgColor
+    if _doneIndicator != nil {
+      doneIndicator.backgroundColor = UIColor.white
+      _doneIndicator = nil
+    }
   }
   
   override var bounds: CGRect {
