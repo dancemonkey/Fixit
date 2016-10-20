aa8a0078bd6780a5e012bcaa739440cf958c0e69
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-10-20 14:18:03 -0400

    hitlist sort button hooked up and persisting

--- Fixit/HitListVC.swift
+++ Fixit/HitListVC.swift
@@ -9,29 +9,29 @@
 import UIKit
 import CoreData
 
+enum HitListSorts: String {
+  case parentProjectTitle, title, time
+}
+
 class HitListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
   @IBOutlet weak var tableView: UITableView!
+  
+  var customSortString: HitListSorts = .parentProjectTitle
     
   var fetchedResultsController: NSFetchedResultsController<Task>!
+  var primarySortDesc: NSSortDescriptor!
+  var secondarySortDesc: NSSortDescriptor!
+  
+  let prefs = UserDefaults.standard
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     tableView.delegate = self
     tableView.dataSource = self
-    
-    let fetch = NSFetchRequest<Task>(entityName: fetches.Tasks.rawValue)
-    let primarySortDesc = NSSortDescriptor(key: "parentProjectTitle", ascending: true)
-    let secondarySortDesc = NSSortDescriptor(key: "time", ascending: true)
-    let tertiarySortDesc = NSSortDescriptor(key: "creationDate", ascending: false)
-    fetch.sortDescriptors = [primarySortDesc,secondarySortDesc, tertiarySortDesc]
-    fetch.predicate = NSPredicate(format: "time.intValue <= 31 AND completed.boolValue == false", argumentArray: nil)
-    
-    fetchedResultsController = NSFetchedResultsController<Task>(fetchRequest: fetch, managedObjectContext: appDelegate.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
-    fetchedResultsController.delegate = self
-    
-    attemptFetch()
+
+    //attemptFetch()
   }
   
   func attemptFetch() {
@@ -44,7 +44,36 @@ class HitListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, N
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    self.tableView.reloadData()
+    setFRC()
+    attemptFetch()
+  }
+  
+  func setSortDescriptors() {
+    if let sortString = prefs.value(forKey: "hitListSortString") as? String {
+      customSortString = HitListSorts(rawValue: sortString)!
+    }
+    primarySortDesc = NSSortDescriptor(key: customSortString.rawValue, ascending: true)
+    switch customSortString {
+    case .parentProjectTitle:
+      secondarySortDesc = NSSortDescriptor(key: "time", ascending: true)
+    case .title:
+      secondarySortDesc = NSSortDescriptor(key: "time", ascending: true)
+    case .time:
+      secondarySortDesc = NSSortDescriptor(key: "parentProjectTitle", ascending: true)
+    }
+    
+  }
+  
+  func setFRC() {
+    let fetch = NSFetchRequest<Task>(entityName: fetches.Tasks.rawValue)
+    setSortDescriptors()
+    let tertiarySortDesc = NSSortDescriptor(key: "creationDate", ascending: false)
+    fetch.sortDescriptors = [primarySortDesc,secondarySortDesc, tertiarySortDesc]
+    fetch.predicate = NSPredicate(format: "time.intValue <= 31 AND completed.boolValue == false", argumentArray: nil)
+    fetch.sortDescriptors = [primarySortDesc,secondarySortDesc, tertiarySortDesc]
+    fetch.predicate = NSPredicate(format: "time.intValue <= 31 AND completed.boolValue == false", argumentArray: nil)
+    self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetch, managedObjectContext: appDelegate.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+    fetchedResultsController.delegate = self
   }
 
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -59,6 +88,42 @@ class HitListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, N
     let alert = UIAlertController(title: "Hit List", message: "This is a list of all tasks that have an estimated time (in minutes) entered into the time field, of 30 minutes or less. This way you can get a quick look at all of the tasks you have on your list that won't take you long to complete. \n\n If you have a few minutes to spare and want to get something done on your Fixit List, this is the place to look!", preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
     self.present(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sortPressed(sender: UIButton) {
+    let alert = UIAlertController(title: "Sort by...", message: nil, preferredStyle: .actionSheet)
+    let project = UIAlertAction(title: "Project title", style: .default, handler: sort(sender: ))
+    let title = UIAlertAction(title: "Task title", style: .default, handler: sort(sender: ))
+    let time = UIAlertAction(title: "Estimated time", style: .default, handler: sort(sender: ))
+    let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    alert.addAction(project)
+    alert.addAction(title)
+    alert.addAction(time)
+    alert.addAction(cancel)
+    self.present(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func filterPressed(sender: UIButton) {
+    
+  }
+  
+  func sort(sender: UIAlertAction) {
+    switch sender.title! {
+    case "Project title":
+      self.customSortString = .parentProjectTitle
+      prefs.set(HitListSorts.parentProjectTitle.rawValue, forKey: "hitListSortString")
+    case "Task title":
+      self.customSortString = .title
+      prefs.set(HitListSorts.title.rawValue, forKey: "hitListSortString")
+    case "Estimated time":
+      self.customSortString = .time
+      prefs.set(HitListSorts.time.rawValue, forKey: "hitListSortString")
+    default: break
+    }
+    self.fetchedResultsController = nil
+    setFRC()
+    attemptFetch()
+    self.tableView.reloadData()
   }
   
   // MARK: Tableview methods
